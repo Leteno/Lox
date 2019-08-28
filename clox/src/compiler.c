b9fd72165a1af4e6f68d31b3fa93be9a1d7ac3fa
juzhen <changchunjunhao@gmail.com>
2019-08-28 22:21:28 +0800

    Add valueType

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -190,7 +190,7 @@ static void binary() {
 
 static void number() {
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
 }
 
 static void consume(TokenType type, const char* message) {
