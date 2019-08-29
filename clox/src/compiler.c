8ad4b61716e09cc13180ea4428f4e700673fc149
juzhen <changchunjunhao@gmail.com>
2019-08-29 22:08:53 +0800

    New type String, support alloc and concat, dirty Makefile

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -44,6 +44,7 @@ static void unary();
 static void binary();
 static void literal();
 static void number();
+static void string();
 
 ParseRule rules[] = {
     { grouping, NULL,    PREC_NONE },       // TOKEN_LEFT_PAREN
@@ -66,7 +67,7 @@ ParseRule rules[] = {
     { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS
     { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS_EQUAL
     { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER
-    { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING
+    { string,     NULL,    PREC_NONE },       // TOKEN_STRING
     { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER
     { NULL,     NULL,    PREC_NONE },       // TOKEN_AND
     { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS
@@ -209,6 +210,11 @@ static void literal() {
 static void number() {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
+}
+
+static void string() {
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
+                                    parser.previous.length - 2)));
 }
 
 static void consume(TokenType type, const char* message) {
